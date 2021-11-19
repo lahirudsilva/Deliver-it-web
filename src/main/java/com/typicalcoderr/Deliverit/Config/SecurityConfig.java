a0cd878aaeb9f760e8efe2c85271a4aa633a02a1
Lahiru De Silva <desilvalahiru4@gmail.com>
2021-11-19 21:57:31 +0530

    [+]spring security configuration

--- src/main/java/com/typicalcoderr/Deliverit/Config/SecurityConfig.java
+++ src/main/java/com/typicalcoderr/Deliverit/Config/SecurityConfig.java
@@ -1,37 +1,59 @@
 package com.typicalcoderr.Deliverit.Config;
 
+import com.typicalcoderr.Deliverit.Service.UserDetailsServiceImplementation;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableWebSecurity
+@AllArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 
+    private UserDetailsServiceImplementation myUserDetailsService;
+
+
+    @Bean
     @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
-    }
 
-    @Override
+    @Autowired
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configure(auth);
+        auth.userDetailsService(myUserDetailsService).passwordEncoder(passwordEncoder());
     }
 
     @Bean
-    PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api/register/**").permitAll()
+                .antMatchers("/api/auth/login").permitAll()
+                .antMatchers("/images/**").permitAll()
+                .antMatchers("/css/**").permitAll()
+                .antMatchers("/util/**").permitAll()
+                .antMatchers("/login*").permitAll()
+                .anyRequest()
+                .authenticated().and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll();
 
+    }
 }
