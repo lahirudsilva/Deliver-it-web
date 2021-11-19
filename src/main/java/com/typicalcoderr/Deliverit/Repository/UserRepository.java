e337250f792b86f89df46a1259c7018cc981633f
Lahiru De Silva <desilvalahiru4@gmail.com>
2021-11-19 21:55:18 +0530

    [+]find by email method added

--- src/main/java/com/typicalcoderr/Deliverit/Repository/UserRepository.java
+++ src/main/java/com/typicalcoderr/Deliverit/Repository/UserRepository.java
@@ -1,7 +1,12 @@
 package com.typicalcoderr.Deliverit.Repository;
 
-import com.typicalcoderr.Deliverit.Model.User;
+import com.typicalcoderr.Deliverit.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findUserByEmail(String email);
 }
