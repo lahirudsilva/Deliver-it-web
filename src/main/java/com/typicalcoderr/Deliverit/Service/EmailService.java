1fe0e68ef7b4f24565de0890b269abdd2b59ae55
Lahiru De Silva <desilvalahiru4@gmail.com>
2021-11-27 16:22:18 +0530

    [+]  email will be sent to customer when rejecting the delivery request

---
+++ src/main/java/com/typicalcoderr/Deliverit/Service/EmailService.java
@@ -0,0 +1,68 @@
+package com.typicalcoderr.Deliverit.Service;
+
+import com.typicalcoderr.Deliverit.dto.ShipmentDto;
+import lombok.AllArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Lahiru
+ * Date: Fri
+ * Time: 12:58 PM
+ */
+@Component
+@AllArgsConstructor
+public class EmailService {
+        private final JavaMailSender emailSender;
+
+
+        public void sendSimpleMessage(ShipmentDto dto) throws MessagingException{
+
+            MimeMessage mimeMessage = emailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
+
+//            String sender = dto.getReceiverEmail();
+
+
+            //Body of email. In HTML format including CSS
+            String htmlMsg = "<html>\n"+
+                    "  <head>\n" +
+                    "    <style>\n" +
+                    "    </style>\n" +
+                    "  </head>\n" +
+                    "  <body>\n" +
+                    "    <h2>Message from Deliverit</h2>\n" +
+                    "\n" +
+                    "    <table>\n" +
+                    "      <tr>\n" +
+                    "        <th>Email of Sender</th>\n" +
+                    "        <td class=\"ln\"><b>"+dto.getSenderEmail()+"</b></td>\n" +
+                    "      </tr>\n" +
+                    "      <tr>\n" +
+                    "        <th>Body</th>\n" +
+                    "        <td>"+"Your Shipment has been Rejected"+"</td>\n" +
+                    "      </tr>\n" +
+                    "    </table>\n" +
+                    "  </body>\n" +
+                    "</html>";
+            try {
+                // Set other attributes
+                helper.setText(htmlMsg, true);
+                helper.setTo(dto.getSenderEmail());
+                helper.setTo(dto.getReceiverEmail());
+                helper.setSubject("\t&copy; Deliverit Notification service");
+                helper.setFrom("thebloginofficial@gmail.com");
+
+
+            } catch (MessagingException e){
+                e.printStackTrace();
+                throw e;
+            }
+            emailSender.send(mimeMessage);
+        }
+}
---
+++ src/main/webapp/WEB-INF/modals/rejectShipment.jsp
@@ -0,0 +1,49 @@
+<div
+        class="modal fade"
+        id="RejectShipment"
+        tabindex="-1"
+        role="dialog"
+        aria-labelledby="exampleModalLabel2"
+        aria-hidden="true"
+>
+    <div class="modal-dialog modal-dialog-centered">
+
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel2">Shipment Rejection
+                    - ${requests.getShipmentId()}</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form method="POST" action="/rejectShipment">
+                    <input hidden id="editIdInput${requests.getShipmentId()}" name="shipmentId"
+                           value="${requests.getShipmentId()}"/>
+                    <input hidden id="editIdInput${requests.getReceiverEmail()}" name="receiverEmail"
+                           value="${requests.getReceiverEmail()}"/>
+                    <input hidden id="editIdInput${requests.getSenderEmail()}" name="senderEmail"
+                           value="${requests.getSenderEmail()}"/>
+                    <div class="mb-3">
+                        <label class="col-form-label"><strong> Recipients: </strong></label>
+                        <span><span class="badge bg-secondary"> ${requests.getReceiverEmail()}</span> ,
+                            <span class="badge bg-secondary">${requests.getSenderEmail()} </span>  </span>
+
+                    </div>
+                    <div class="mb-3">
+                        <label for="message-text" class="col-form-label"> <strong>Message:</strong></label>
+                        <textarea class="form-control" id="message-text" rows="5" required></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-success" style="margin-left: 170px;">Send Response</button>
+                </form>
+                <button class="btn btn-primary" data-bs-target="#AssignDriverModal${requests.getShipmentId()}"
+                        data-bs-toggle="modal"
+                        data-bs-dismiss="modal">Back to Assign
+                </button>
+            </div>
+            <div class="modal-footer">
+
+            </div>
+        </div>
+
+
+    </div>
+</div>
