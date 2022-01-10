bd6a2c64a4162d81ef1ac64044b6e2709d4c6cf4
lahiru de silva <desilvalahiru4@gmail.com>
2022-01-10 15:47:28 +0530

    [+] test cases for customer

---
+++ src/test/java/com/typicalcoderr/Deliverit/CustomerServiceTests.java
@@ -0,0 +1,138 @@
+package com.typicalcoderr.Deliverit;
+
+import com.typicalcoderr.Deliverit.Service.CustomerService;
+import com.typicalcoderr.Deliverit.domain.User;
+import com.typicalcoderr.Deliverit.dto.UserDto;
+import com.typicalcoderr.Deliverit.exceptions.DeliveritException;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Lahiru
+ * Date: Sun
+ * Time: 7:48 PM
+ */
+
+//@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CustomerServiceTests{
+
+    @Autowired
+    private CustomerService customerService;
+
+    @Autowired
+    private TestUtil testUtil;
+
+    private String customerToBeDeleted, customerWithSameEmail;
+
+//    @BeforeAll
+//    public void init() throws DeliveritException{
+//        System.out.println("hrllo");
+//        customerToBeDeleted = testUtil.createCustomerToBeDeleted();
+//        customerWithSameEmail = testUtil.createCustomerWithSameEmail();
+//        System.out.println("sd"+customerWithSameEmail);
+//    }
+
+
+
+
+
+    @Test
+    public void testRegisterCustomer() throws DeliveritException{
+        UserDto dto = new UserDto();
+        dto.setEmail("test@AddCustomer");
+        dto.setFirstName("First");
+        dto.setLastName("Last");
+        dto.setCity("testCity");
+        dto.setContactNumber("1231231232");
+        dto.setPassword("test");
+
+
+        User result = customerService.registerCustomer(dto);
+
+        assertNotNull(result);
+
+        System.out.println("[TEST] Adding a customer [PASSED]");
+
+
+    }
+
+    @Test
+    public void testAddCustomerWithExistingEmail() throws DeliveritException {
+
+        customerWithSameEmail = testUtil.createCustomerWithSameEmail();
+
+//        customerWithSameEmail = testUtil.createCustomerWithSameEmail();
+//        System.out.println(customerWithSameEmail);
+        UserDto customerDto = new UserDto();
+        customerDto.setEmail(customerWithSameEmail);
+        customerDto.setFirstName("First");
+        customerDto.setLastName("Last");
+        customerDto.setCity("testCity");
+        customerDto.setContactNumber("1231231232");
+        customerDto.setPassword("test");
+
+        boolean isTrue = false;
+
+        try {
+            customerService.registerCustomer(customerDto);
+
+        } catch (DeliveritException e) {
+            if (e.getMessage().equals("Email already exists!")) isTrue = true;
+        }
+
+        assertTrue(isTrue);
+
+        System.out.println("[TEST] Attempt to register customer with existing email [PASSED]");
+
+    }
+
+    @Test
+    public void testGetAllCustomers() throws DeliveritException {
+        List<UserDto> results = customerService.getAllCustomers();
+        System.out.println(results);
+
+        boolean isTrue = results.size() > 0;
+
+        assertTrue(isTrue);
+
+        System.out.println("[TEST] Get all students [PASSED]");
+
+    }
+
+    @Test
+    public void testDeleteCustomer() throws DeliveritException {
+        customerToBeDeleted = testUtil.createCustomerToBeDeleted();
+
+        customerService.removeCustomer(customerToBeDeleted);
+
+        List<UserDto> results = customerService.getAllCustomers();
+
+        boolean isTrue = true;
+
+        for (UserDto dto : results) {
+            if (dto.getEmail() == customerToBeDeleted) {
+                isTrue = false;
+                break;
+            }
+        }
+
+        assertTrue(isTrue);
+
+        System.out.println("[TEST] Delete student [PASSED]");
+    }
+
+}
