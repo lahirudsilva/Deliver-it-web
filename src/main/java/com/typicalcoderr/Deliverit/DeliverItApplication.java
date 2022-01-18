4a30f2fc8def3f3c2e9d39803f8eca363ec6b8d7
lahiru de silva <desilvalahiru4@gmail.com>
2022-01-18 13:05:40 +0530

    [+] run methods asynchronously

--- src/main/java/com/typicalcoderr/Deliverit/DeliverItApplication.java
+++ src/main/java/com/typicalcoderr/Deliverit/DeliverItApplication.java
@@ -2,8 +2,14 @@ package com.typicalcoderr.Deliverit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
 
 @SpringBootApplication
+@EnableAsync
 public class DeliverItApplication {
 
 	public static void main(String[] args) {
@@ -11,6 +17,17 @@ public class DeliverItApplication {
 		SpringApplication.run(DeliverItApplication.class, args);
 
 		System.out.println("Starting.........");
+	}
+
+	@Bean
+	public Executor taskExecutor() {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(2);
+		executor.setMaxPoolSize(2);
+		executor.setQueueCapacity(500);
+		executor.setThreadNamePrefix("Async-");
+		executor.initialize();
+		return executor;
 	}
 
 }
